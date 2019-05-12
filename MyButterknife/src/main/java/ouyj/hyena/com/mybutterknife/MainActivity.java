b5712162ca7084daa6a7ca237a81c1468b2ae149
joyoverflow <joyoverflow@sina.com>
2019-05-12 22:40:31 +0800

    优化自定义适配器类

--- MyButterknife/src/main/java/ouyj/hyena/com/mybutterknife/MainActivity.java
+++ MyButterknife/src/main/java/ouyj/hyena/com/mybutterknife/MainActivity.java
@@ -21,10 +21,14 @@ import butterknife.OnLongClick;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final ButterKnife.Action<View> ALPHA_FADE = new ButterKnife.Action<View>() {
         @Override public void apply(@NonNull View view, int index) {
+            //设置动画
             AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
             alphaAnimation.setFillBefore(true);
             alphaAnimation.setDuration(500);
@@ -38,9 +42,11 @@ public class MainActivity extends AppCompatActivity {
     TextView subtitle;
     @BindView(R.id.hello)
     Button hello;
-    @BindView(R.id.list_of_things)
-    ListView listOfThings;
-    @BindView(R.id.footer) TextView footer;
+    @BindView(R.id.listView)
+    ListView listView;
+    @BindView(R.id.footer)
+    TextView footer;
+
     @BindViews({ R.id.title, R.id.subtitle, R.id.hello })
     List<View> headerViews;
 
@@ -51,25 +57,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //将黄油刀绑定到活动
         ButterKnife.bind(this);
 
+        //设置视图文本
         title.setText("Butter Knife");
         subtitle.setText("Field and method binding for Android views.");
         footer.setText("by Jake Wharton");
         hello.setText("Say Hello");
+
+        //为列表视设置适配器
         adapter = new SimpleAdapter(this);
-        listOfThings.setAdapter(adapter);
+        listView.setAdapter(adapter);
     }
 
-    @OnClick(R.id.hello) void sayHello() {
+    @OnClick(R.id.hello)
+    void sayHello() {
         Toast.makeText(this, "Hello, views!", LENGTH_SHORT).show();
+
+        //执行动画
         ButterKnife.apply(headerViews, ALPHA_FADE);
     }
-    @OnLongClick(R.id.hello) boolean sayGetOffMe() {
+    @OnLongClick(R.id.hello)
+    boolean sayGetOffMe() {
         Toast.makeText(this, "Let go of me!", LENGTH_SHORT).show();
         return true;
     }
-    @OnItemClick(R.id.list_of_things) void onItemClick(int position) {
-        Toast.makeText(this, "You clicked: " + adapter.getItem(position), LENGTH_SHORT).show();
+    @OnItemClick(R.id.listView)
+    void onItemClick(int position) {
+        Toast.makeText(
+                this,
+                "You clicked: " + adapter.getItem(position),
+                LENGTH_SHORT
+        ).show();
     }
 }
